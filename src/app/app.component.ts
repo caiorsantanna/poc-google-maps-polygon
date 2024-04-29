f96c09fde885511637a5f0d25740093da8e28305
Caio Santanna <caiorsantanna@gmail.com>
2024-04-29 11:38:41 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    RouterOutlet,
+    GoogleMapsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'poc-google-maps';
+  public center: google.maps.LatLngLiteral = {lat: 24, lng: 12};
+  public zoom = 4;
+  public vertices = signal<google.maps.LatLngLiteral[]>([]);
+  public options: google.maps.PolygonOptions = {
+    draggable: true,
+    editable: true
+  }
+
+  constructor (
+  ) { }
+
+  public onClick(event: google.maps.MapMouseEvent): void {
+    this.vertices.update(vertices => {
+      if (!event.latLng) { return vertices; }
+
+      return [...vertices, event.latLng.toJSON()];
+    })
+  }
+
+  public test(event: any) {
+    console.log(event)
+  }
 }
